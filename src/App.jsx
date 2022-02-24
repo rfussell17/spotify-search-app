1e58137f88f4453c12987749e31853b5ea5a70f6
robin <rfussell17@gmail.com>
2022-02-24 12:17:53 -0800

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
+import Nav from './components/Nav';
 import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +22,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  
+  )
 }
 
 export default App;
