874a3c4d5c385d72b4841d046cc98fd3e8d4ac52
robin <rfussell17@gmail.com>
2022-02-24 11:32:38 -0800

    create component folder

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          SpotiFind
+          Spoti<span>FIND</span>
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Listen Now
+          Discover New Music Today
         </a>
       </header>
     </div>
