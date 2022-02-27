e926f3b89d91a4380c00caacc20a664e8aa6a47f
robin <rfussell17@gmail.com>
2022-02-27 09:31:33 -0800

    mod hero text

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -4,7 +4,10 @@ export default function MainContent() {
   return (
 		<div className="main-content bg-dark ">
 			<div className="main-content-title">
-				<h2>Revisit old favourites, or discover new ones.</h2>
+				<div className="main-content-title-bg">
+				<h2><span className='text-accent'>Celebrating</span> old favourites.</h2>
+					<h2><span className='text-accent'>Discovering</span> new ones.</h2>
+				</div>
 			</div>
 		</div>
 	)
