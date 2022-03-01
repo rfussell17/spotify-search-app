6f6a55163d6f7caaac99b790385b98407ce36869
robin <rfussell17@gmail.com>
2022-02-28 19:05:59 -0800

    reformat nav code

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,9 +5,9 @@ export default function Navbar() {
     return (
         <nav>
             <h3 className="nav--logo_text">
- 						<i class="fa-solid fa-waveform-lines nav--icon"></i>SpotiFIND</h3>
+                <i class="fa-solid fa-waveform-lines nav--icon"></i>SpotiFIND</h3>
             <h5 className="nav--link">Search</h5>
-						<h5 className="nav--link">Discover</h5>
+            <h5 className="nav--link">Discover</h5>
         </nav>
     )
 }
