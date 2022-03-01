c894d658e6c3f77513ee28d4103e3b129dd041c8
robin <rfussell17@gmail.com>
2022-02-28 19:05:04 -0800

    update hero text

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -5,7 +5,7 @@ export default function MainContent() {
 		<div className="main-content bg-dark ">
 			<div className="main-content-title">
 				<div className="main-content-title-bg">
-				<h2><span className='text-accent'>Celebrating</span> old favourites.</h2>
+				<h2><span className='text-accent'>Celebrating</span> your favourite songs.</h2>
 					<h2><span className='text-accent'>Discovering</span> new ones.</h2>
 				</div>
 			</div>
