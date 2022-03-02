0b3f18713b17facb4c197914ca17ea07c9f0e71d
robin <rfussell17@gmail.com>
2022-03-01 22:01:26 -0800

    fix header tag typo

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Search() {
   return (
 		<div className="search-body bg-dark ">
-		<h1>Search box and results go here.</h1>
+		<h1>Search box and results go here</h1>
 		</div>
 	)
 }
