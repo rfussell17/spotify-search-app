e09d0c5f25850d42ae343c233bbaf2ea0f3f56e8
robin <rfussell17@gmail.com>
2022-03-02 14:34:28 -0800

    remove dead imports

--- src/App.jsx
+++ src/App.jsx
@@ -5,40 +5,39 @@ import Discover from './components/Discover';
 import Search from './components/Search';
 import './App.css';
 
-import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
-  return (
-    
-    <Router className="App bg-dark"> 
-      <header className="App-header bg-dark">
-        <Nav />
-      </header>
+	return (
 
-      <main>
-      <div className="main-content bg-dark ">
-				<div className="main-content-title-bg">
-   
-      <Route path="/home">
-        <MainContent />
-      </Route>
-      <Route path="/discover">
-        <Discover />
-      </Route>
-      <Route path="/search">
-        <Search />
-      </Route>
-      </div>
-      </div>
-      </main>
+		<Router className="App bg-dark">
 
+			<header className="App-header bg-dark">
+				<Nav />
+			</header>
 
-      <footer>
-      <Footer />
-      </footer>
-    </Router>
-  
-  )
+			<main>
+				<div className="main-content bg-dark ">
+					<div className="main-content-title-bg">
+						<Route path="/home">
+							<MainContent />
+						</Route>
+						<Route path="/discover">
+							<Discover />
+						</Route>
+						<Route path="/search">
+							<Search />
+						</Route>
+					</div>
+				</div>
+			</main>
+
+			<footer>
+				<Footer />
+			</footer>
+
+		</Router>
+	)
 }
 
 export default App;
