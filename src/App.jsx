d31d2536f92713269da32789f0706f3edd395bc4
robin <rfussell17@gmail.com>
2022-03-02 14:18:13 -0800

    implementing React Router NavLink

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import MainContent from './components/MainContent';
+import Discover from './components/Discover';
+import Search from './components/Search';
 import './App.css';
+
+import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
 
 function App() {
   return (
     
-    <div className="App bg-dark"> 
+    <Router className="App bg-dark"> 
       <header className="App-header bg-dark">
         <Nav />
       </header>
-      <MainContent />
+      <Route path="/home">
+        <MainContent />
+      </Route>
+      <Route path="/discover">
+        <Discover />
+      </Route>
+      <Route path="/search">
+        <Search />
+      </Route>
+      
       <Footer />
-    </div>
+    </Router>
   
   )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,14 @@
 
 import React from "react"
+import { NavLink } from "react-router-dom"
 
 export default function Navbar() {
     return (
         <nav>
-            <h3 className="nav--logo_text">
-                <i class="fa-solid fa-waveform-lines nav--icon"></i>SpotiFIND</h3>
-            <h5 className="nav--link">Search</h5>
-            <h5 className="nav--link">Discover</h5>
+            <h3 className="nav--logo_text"><NavLink to="/home">
+                <i class="fa-solid fa-waveform-lines nav--icon"></i>SpotiFIND</NavLink></h3>
+            <h5 className="nav--link"><NavLink to="/search">Search</NavLink></h5>
+            <h5 className="nav--link"><NavLink to="/discover">Discover</NavLink></h5>
         </nav>
     )
 }
