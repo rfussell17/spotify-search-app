aefb9e66314224d2c3e0c3d09f0c93c9ff287557
robin <rfussell17@gmail.com>
2022-03-06 09:01:04 -0800

    render card img

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 export default function Search() {
   return (
-		<div className="main-content-title">
-		<h2><span className='text-accent'>Search</span> Goes here</h2>
+		<div className="container">
+			<div className="main-content-title">
+			<h2><span className='text-accent'>Search</span> Goes here</h2>
+			</div>
+
+			<div className="card mb-3 bg-dark">
+  <img src="./src/images/artist.jpeg" className="card-img-top" alt="album cover"/>
+
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+</div>
+
 		</div>
+
 	)
 }
