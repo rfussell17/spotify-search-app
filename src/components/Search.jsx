38995e02d9f6982807277675885d736ad69c68de
robin <rfussell17@gmail.com>
2022-03-11 08:58:37 -0800

    add search bar, 3 radio buttons to search page

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 export default function Search() { 
   return (
 		<div className="container search-container">
+
+
+
 			<div className="main-content-title artist-title">
 				<h2><span className='text-accent'>Search</span> millions of artists and songs</h2>
 
@@ -16,6 +19,47 @@ export default function Search() {
 					</div>
 				</div>
 			</div>
+
+
+
+
+{/* <div className="form-check form-check-inline">
+  <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"></input>
+  <label className="form-check-label" for="inlineRadio1">Song</label>
+</div>
+
+
+<div className="form-check form-check-inline">
+  <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"></input>
+  <label class="form-check-label" for="inlineRadio2">Artist</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" ></input>
+  <label class="form-check-label" for="inlineRadio3">Album</label>
+</div> */}
+
+
+
+<div className="input-group bg-dark">
+<input type="text" className="form-control bg-dark text-light text-muted" aria-label="Text input with radio button"></input>
+  <div className="input-group-text bg-dark text-light">
+    <input className="form-check-input mt-0" type="radio" value="" id="song" aria-label="Radio button for song category"></input>
+		<label className="form-check-label" for="song"><span className="radio-span">Song</span></label>
+  </div>
+	<div className="input-group-text bg-dark text-light">
+    <input className="form-check-input mt-0" type="radio" value="" id="artist" aria-label="Radio button for artist category"></input>
+		<label className="form-check-label" for="artist"> <span className="radio-span">Artist</span></label>
+  </div>
+	<div className="input-group-text bg-dark text-light">
+    <input className="form-check-input mt-0" type="radio" value="" id="album" aria-label="Radio button for album category"></input>
+		<label className="form-check-label" for="album"><span className="radio-span">Album</span></label>
+  </div>
+
+</div>
+
+
+
+
 
 			<div className="card mb-3 artist-card bg-dark">
   <div className="row g-0">
