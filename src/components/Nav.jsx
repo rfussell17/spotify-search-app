bda64d9ed9d41cc1a6376dc8a28783ff0929ba6e
robin <rfussell17@gmail.com>
2022-03-11 09:28:49 -0800

    fix logo icon + text display on mobile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     return (
         <nav>
             <h3 className="nav--logo_text"><NavLink to="/home">
-                <i class="fa-solid fa-waveform-lines nav--icon"></i>SpotiFIND</NavLink></h3>
+                <i class="fa-solid fa-waveform-lines nav--icon"></i><span id="logo-text">SpotiFIND</span></NavLink></h3>
             <h5 className="nav--link"><NavLink to="/search">Search</NavLink></h5>
             <h5 className="nav--link"><NavLink to="/discover">Discover</NavLink></h5>
         </nav>
